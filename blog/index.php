bed0f80cd78164cada8d018ddb5720d1961fda6a
AHOUANDOGBO Amen <112037382+Jores02@users.noreply.github.com>
2024-10-22 08:40:43 +0200

    "Base pour afficher les commentaires

--- blog/index.php
+++ blog/index.php
@@ -1,12 +1,7 @@
 <?php
-require('src/model.php');
-$posts = getPosts();
-require('templates/homepage.php');
- /* défaut du code :
-            1: utilisation du die au lieu du catch 
-            2: aucun filtre 
-            3: PHP et html mélanger
-               
-            */ 
-?>
 
+require('src/model.php');
+
+$posts = getPosts();
+
+require('templates/homepage.php');
--- blog/src/model.php
+++ blog/src/model.php
@@ -1,27 +1,25 @@
 <?php
+
 function getPosts() {
-// We connect to the database.
-try {
-$database = new PDO('mysql:host=localhost;dbname=blog;charset=utf8',
-'root', 'root');
-} catch(Exception $e) {
-die('Erreur : '.$e->getMessage());
+    try {
+        $database = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', 'root');
+    } catch(Exception $e) {
+        die('Erreur : '.$e->getMessage());
+    }
+
+    $statement = $database->query(
+        "SELECT id, title, content, DATE_FORMAT(creation_date, '%d/%m/%Y à %Hh%imin%ss') AS french_creation_date FROM posts ORDER BY creation_date DESC LIMIT 0, 5"
+    );
+    $posts = [];
+    while (($row = $statement->fetch())) {
+        $post = [
+            'title' => $row['title'],
+            'french_creation_date' => $row['french_creation_date'],
+            'content' => $row['content'],
+        ];
+
+        $posts[] = $post;
+    }
+
+    return $posts;
 }
-// We retrieve the 5 last blog posts.
-$statement = $database->query(
-"SELECT id, titre, contenu, DATE_FORMAT(date_creation,
-'%d/%m/%Y à %Hh%imin%ss') AS date_creation_fr FROM billets
-ORDER BY date_creation DESC LIMIT 0, 5"
-);
-$posts = [];
-while (($row = $statement->fetch())) {
-$post = [
-'title' => $row['titre'],
-'french_creation_date' => $row['date_creation_fr'],
-'content' => $row['contenu'],
-];
-$posts[] = $post;
-}
-return $posts;
-}
-?>
